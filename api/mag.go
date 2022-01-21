b3a14082cbc221a302d38b8b93568e5f146c6c67
brillience <360727078@qq.com>
2022-01-21 13:08:50 +0800

    update doc

--- api/mag.go
+++ api/mag.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/tal-tech/go-zero/rest/httpx"
+	"mag_service/common/errorx"
+	"net/http"
 
 	"mag_service/api/internal/config"
 	"mag_service/api/internal/handler"
@@ -25,6 +28,16 @@ func main() {
 	defer server.Stop()
 
 	handler.RegisterHandlers(server, ctx)
+
+	// 自定义错误
+	httpx.SetErrorHandler(func(err error) (int, interface{}) {
+		switch e := err.(type) {
+		case *errorx.CodeError:
+			return http.StatusOK, e.Data()
+		default:
+			return http.StatusInternalServerError, nil
+		}
+	})
 
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
 	server.Start()
